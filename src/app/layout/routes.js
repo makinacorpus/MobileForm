aa575453c6f95430119783676d9f08103317d8d7
Simon Prévidente <simon.previdente@free.fr>
2015-04-16 10:20:28 +0200

    Add header & content templates

--- src/app/layout/routes.js
+++ src/app/layout/routes.js
@@ -3,15 +3,27 @@
 var controller = require('./controllers');
 
 function layoutRoutes($stateProvider, $urlRouterProvider) {
-  console.log('layoutRoutes ok')
+	console.log('layoutRoutes ok')
 
 	$urlRouterProvider.otherwise('/');
 
 	$stateProvider
 		.state('layout', {
+			abstract: true,
 			url: '/',
 			template: require('./templates/layout.html'),
 			controller: controller.LayoutController
+		})
+		.state('layout.home', {
+			url: '',
+			views: {
+				'header' : {
+					template: require('./templates/header.html')
+				},
+				'content' : {
+					template: require('./templates/content.html')
+				}
+			}
 		});
 }
 
